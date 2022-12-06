0eceaab55fbb1b2aea4a6898e60b590d2d510574
robertsarafie <sarafierobert@gmail.com>
2022-12-06 22:00:13 +0200

    s04 ex06-09 - clasa

---
+++ m01s04/ex08/app.js
@@ -0,0 +1,24 @@
+var i = 1;
+
+// while (i <= 100) {
+//   console.log(i);
+
+//   i++;
+// }
+
+// while (i < 100) {
+//   console.log(i);
+
+//   i++;
+// }
+
+while (i < 100) {
+  console.log(i);
+
+  if (i === 50) {
+    i++;
+    break;
+  }
+
+  i++;
+}
---
+++ m01s04/ex09/app.js
@@ -0,0 +1,25 @@
+var i = 1;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 100);
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i < 100);
+
+// do {
+//   i++;
+// } while (i < 100);
+
+do {
+  i++;
+
+  console.log(i);
+
+  if (i === 50) {
+    break;
+  }
+} while (i < 100);
